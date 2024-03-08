0390bb5d3dab9434ea429fe535a2faf2ad072872
Erin Nurfajrina <160722209+Meirinn11@users.noreply.github.com>
2024-03-08 10:27:50 +0700

    Add files via upload

---
+++ Menginput nilai.py
@@ -0,0 +1,8 @@
+nilai = int (input("masukan nilai anda: "))
+
+if nilai >75:
+    hasil ="alhamdulilah wasyukurillah anda dinyatakan lulus"
+else:
+    hasil ="cieee anda tidak lulus semangat yaa"
+    
+print(hasil)
